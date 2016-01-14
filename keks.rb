8fdf50b35ed63303905de30434e6a762d74e7b80
Jeff Sandberg <paradox460@gmail.com>
2016-01-14 11:49:52 -0800

    more keks

--- keks.rb
+++ keks.rb
@@ -1,11 +1,21 @@
 require 'sinatra/base'
+require 'json'
 
 class Keks < Sinatra::Base
-  get %r(/(\d+)(\.json)?) do |amount, json|
-    keks(amount, json)
+  get %r{/stream} do
+  content_type :txt
+  stream do |out|
+    loop { out << "kek\n" }
+  end
   end
 
-  get %r(/(\.json)?) do |json|
+  get %r{/(\d+)(\.json)?} do |amount, json|
+    content_type(json.nil? ? :txt : :json)
+    keks(amount.to_i, json)
+  end
+
+  get %r{/(\.json)?} do |json|
+    content_type(json.nil? ? :txt : :json)
     keks(rand(100), json)
   end
 
@@ -13,10 +23,11 @@ class Keks < Sinatra::Base
     keks(rand(100))
   end
 
-  run! if app_file == $0
+  run! if app_file == $PROGRAM_NAME
 
   private
-  def keks(amount = 1, json=nil)
+
+  def keks(amount = 1, json = nil)
     k = ['kek'] * amount
     return k.to_json if json
     k.join(' ')
